115092f757e34e62f1fea5d8bb01547487581c5d
mohammed5420 <mohamed.elbadwi.dev@gmail.com>
2023-09-06 20:27:26 +0300

    Changed Nvim theme to catppuccin :D

--- nvim/init.lua
+++ nvim/init.lua
@@ -192,10 +192,10 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    "catppuccin/nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
@@ -206,7 +206,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
