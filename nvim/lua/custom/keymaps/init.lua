b68f59f3309ebf90fad4eefe030de9873d653187
mohammed5420 <mohamed.elbadwi.dev@gmail.com>
2023-09-16 07:52:28 +0300

    Added rempas to move selected text

--- nvim/lua/custom/keymaps/init.lua
+++ nvim/lua/custom/keymaps/init.lua
@@ -44,3 +44,7 @@ vim.keymap.set('n', 'wq', '<C-w>q', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Remap For Moving Text while hilighting
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
