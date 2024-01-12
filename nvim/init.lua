f37781f98170f62ebac7a6b5d4dd219b139deea7
mohammed5420 <mohamed.elbadwi.dev@gmail.com>
2024-01-12 05:57:34 +0200

    changed indentation rules

--- nvim/init.lua
+++ nvim/init.lua
@@ -308,6 +308,13 @@ vim.o.scrolloff = 8
 -- Set Relative numbers
 vim.o.relativenumber = true
 
+-- indent
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+vim.o.smartindent = true
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
