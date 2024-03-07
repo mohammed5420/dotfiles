eac55c367358b1beabc58704d582977a499f6373
mohammed5420 <mohamed.elbadwi.dev@gmail.com>
2024-03-07 07:24:51 +0200

    added harpoon

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -1,4 +1,10 @@
 return {
-	'nvim-lua/plenary.nvim',
-	'ThePrimeagen/harpoon'
+	'ThePrimeagen/harpoon',
+	config = function()
+		vim.keymap.set('n', '<leader>z', function() require('harpoon.mark').add_file() end)
+		local ui = require('harpoon.ui')
+		vim.keymap.set('n', '<leader>ha', function() ui.toggle_quick_menu() end)
+		vim.keymap.set('n', '<C-h>', function() ui.nav_file(1) end)
+		vim.keymap.set('n', '<C-t>', function() ui.nav_file(2) end)
+	end
 }
